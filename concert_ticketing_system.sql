a962a4d80eb5e264d4ff801703b53006faeb7fbf
Jose Simbillo <144484430+GithubSim13@users.noreply.github.com>
2024-11-19 11:37:27 +0800

    Update concert_ticketing_system.sql

--- concert_ticketing_system.sql
+++ concert_ticketing_system.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `concerttix`;
 USE `concerttix`;
 
 CREATE TABLE IF NOT EXISTS Companies (
-    company_code VARCHAR(10) PRIMARY KEY,
+    company_code INT PRIMARY KEY,
     company_name VARCHAR(50),
     address_line1 VARCHAR(50),
     address_line2 VARCHAR(50),
@@ -12,25 +12,25 @@ CREATE TABLE IF NOT EXISTS Companies (
     country VARCHAR(50),
     postal_code VARCHAR(10),
     company_email VARCHAR(50),
-    company_contact_number VARCHAR(15),
+    company_contact_number VARCHAR(10),
     status ENUM('active', 'dissolved')
 );
 
 CREATE TABLE IF NOT EXISTS Artists (
-    artist_code VARCHAR(10) PRIMARY KEY,
-    company_code VARCHAR(10),
+    artist_code INT PRIMARY KEY,
+    company_code INT,
     stage_name VARCHAR(50),
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     birth_date DATETIME,
     email VARCHAR(50),
-    contact_number VARCHAR(15),
+    contact_number VARCHAR(10),
     nationality VARCHAR(50),
     FOREIGN KEY (company_code) REFERENCES Companies(company_code)
 );
 
 CREATE TABLE IF NOT EXISTS Venues (
-    venue_code VARCHAR(10) PRIMARY KEY,
+    venue_code INT PRIMARY KEY,
     venue_name VARCHAR(50),
     address_line1 VARCHAR(50),
     street VARCHAR(50),
@@ -38,14 +38,13 @@ CREATE TABLE IF NOT EXISTS Venues (
     province VARCHAR(50),
     barangay VARCHAR(50),
     postal_code VARCHAR(10),
-    total_seating_capacity INT,
-    total_available_seats INT
+    total_seating_capacity INT
 );
 
 CREATE TABLE IF NOT EXISTS Concerts (
-    concert_code VARCHAR(10) PRIMARY KEY,
-    artist_code VARCHAR(10),
-    venue_code VARCHAR(10),
+    concert_code INT PRIMARY KEY,
+    artist_code INT,
+    venue_code INT,
     performer_name VARCHAR(50),
     genre VARCHAR(50),
     entry_restrictions ENUM('G', '18+'),
@@ -59,31 +58,31 @@ CREATE TABLE IF NOT EXISTS Concerts (
 );
 
 CREATE TABLE IF NOT EXISTS Customers (
-    customer_code VARCHAR(10) PRIMARY KEY,
+    customer_code INT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(50),
-    contact_number VARCHAR(15)
+    contact_number VARCHAR(10)
 );
 
 CREATE TABLE IF NOT EXISTS Transactions (
-    transaction_code VARCHAR(10) PRIMARY KEY,
-    customer_code VARCHAR(10),
+    transaction_code INT PRIMARY KEY,
+    customer_code INT,
     transaction_type ENUM('buy', 'refund', 'transfer', 'cancel'),
     transaction_status ENUM('open', 'closed'),
     transaction_date DATETIME,
-    total_amount INT,
+    total_amount DECIMAL(10, 2),
     payment_method ENUM('cash', 'bank_transfer', 'card'),
     refund_reason VARCHAR(200),
     FOREIGN KEY (customer_code) REFERENCES Customers(customer_code)
 );
 
 CREATE TABLE IF NOT EXISTS Tickets (
-    ticket_code VARCHAR(10) PRIMARY KEY,
-    concert_code VARCHAR(10),
-    customer_code VARCHAR(10),
-    transaction_code VARCHAR(10),
-    ticket_price INT,
+    ticket_code INT PRIMARY KEY,
+    concert_code INT,
+    customer_code INT,
+    transaction_code INT,
+    ticket_price DECIMAL(10, 2),
     seat_number INT,
     status ENUM('valid', 'refunded', 'transferred'),
     FOREIGN KEY (concert_code) REFERENCES Concerts(concert_code),
